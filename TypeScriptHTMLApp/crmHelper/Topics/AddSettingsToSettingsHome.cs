760b8b09c4e14fcf10acfb432952aa61ca520f0b
Ravi <andy.ravi@gmail.com>
2018-01-19 10:32:04 +0530

    How to crate new Settings in Settings Home

---
+++ TypeScriptHTMLApp/crmHelper/Topics/AddSettingsToSettingsHome.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TypeScriptHTMLApp.crmHelper.Topics
+{
+    public class AddSettingsToSettingsHome
+    {
+        // How to crate new Settings in Settings Home 
+        // TO CRM : Settings>> Settings Home 
+        // Add new Folder "Prospect Profile Settings"
+        // to this new Folder Add Web Likn as "Prospect Profile Settings"
+        // radio button "On" to activate the feature (Allow the prospect to upload a profile picture, that will display on their person record)
+
+
+    }
+}
