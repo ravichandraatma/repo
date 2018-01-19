1743b3da5311318b7788d2de555cc7aaeff75d1e
Ravi <andy.ravi@gmail.com>
2018-01-19 09:35:31 +0530

    Updated for Entity Image work... Loading Image and Display back.. along with CRM user can toggle to see contact pic.

---
+++ TypeScriptHTMLApp/crmHelper/Topics/CustomizeSysFormsAndPublish.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TypeScriptHTMLApp.crmHelper.Topics
+{
+    public class CustomizeSysFormsAndPublish
+    {
+        private void ShowImage()
+        {
+            CrmQuery qe = new CrmQuery("systemform");
+            qe.AddSimpleCondition("type", "2"); //main form
+            qe.AddSimpleCondition("objecttypecode", Person.EntityLogicalName.ToLower());
+            qe.Attributes = new List<string>() { "formxml" };
+            var allSysforms = WebControlUtility.CrmData.RetrieveMultiple<SystemForm>(qe);
+            SystemForm ImageAttributeDemoMainForm = allSysforms.First<SystemForm>();
+
+            XDocument ImageAttributeDemoMainFormXml = XDocument.Parse(ImageAttributeDemoMainForm.formxml);
+            if (ImageAttributeDemoMainFormXml.Root.Attribute("showImage").Value == "true")
+                ImageAttributeDemoMainFormXml.Root.SetAttributeValue("showImage", false);
+            else
+                ImageAttributeDemoMainFormXml.Root.SetAttributeValue("showImage", true);           //Updating the entity form definition
+            ImageAttributeDemoMainForm.formxml = ImageAttributeDemoMainFormXml.ToString();
+            WebControlUtility.CrmData.Update(ImageAttributeDemoMainForm);
+
+            OrganizationServiceContext context = new OrganizationServiceContext(WebControlUtility.CrmData.Connection.OrganizationService);
+            PublishXmlRequest pxReq1 = new PublishXmlRequest { ParameterXml = String.Format(@"
+               <importexportxml>
+                <entities>
+                 <entity>{0}</entity>
+                </entities>
+               </importexportxml>", Person.EntityLogicalName.ToLower()) };
+            context.Execute(pxReq1);
+
+        }
+    }
+}
---
+++ TypeScriptHTMLApp/crmHelper/Topics/workingWithEntityImage.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TypeScriptHTMLApp.crmHelper.Topics
+{
+    public class workingWithEntityImage
+    {
+
+        void uploadBtn_Click(object sender, EventArgs e)
+        {
+            byte[] fileData = null;
+            if (fileUpload != null && fileUpload.PostedFile.ContentLength > 0)
+            {
+                DeleteExistingImage();
+                using (var binaryReader = new BinaryReader(fileUpload.FileContent))
+                    fileData = binaryReader.ReadBytes(fileUpload.PostedFile.ContentLength);
+
+                //----------------------
+                //CrmQuery crmQuery = new CrmQuery(Person.EntityLogicalName);
+                //crmQuery.AddSimpleCondition(Person.AttributeNames.PersonID, LoggedInUserId.Value.ToString());
+                //CoreContact externalAgent = WebControlUtility.CrmData.RetrieveMultiple<Person>(crmQuery).First<Person>();
+                //externalAgent.Attributes["entityimage"] = fileData;
+                //WebControlUtility.CrmData.Update(externalAgent);
+
+                OrganizationServiceContext context = new OrganizationServiceContext(WebControlUtility.CrmData.Connection.OrganizationService);
+                Entity contact = context.CreateQuery(Person.EntityLogicalName).Where(c => c.GetAttributeValue<Guid>(Person.AttributeNames.PersonID)
+                .Equals(LoggedInUserId.Value.ToString())).First<Entity>();
+                contact.Attributes["entityimage"] = fileData;
+                context.UpdateObject(contact);
+                try
+                { context.SaveChanges(); }
+                catch (Exception ex)
+                {
+                    InvalidFileTypesErrorMessage = "Invalid File, Retty with a valid picture file: " + ex.Message;
+                }
+
+                //---------------------------
+                WebControlUtility.CrmData.AddAttachmentToEntity(fileUpload.FileContent, fileUpload.PostedFile.ContentLength, fileUpload.PostedFile.ContentType,
+                    "prospectimageupload_" + fileUpload.FileName, "", Person.EntityLogicalName, LoggedInUserId.Value);
+
+            }
+            CrmQuery query = new CrmQuery(Person.EntityLogicalName);
+            query.Attributes = new List<string>() { "entityimage" };
+            query.AddSimpleCondition(Person.AttributeNames.PersonID, LoggedInUserId.Value.ToString());
+            var attachments = WebControlUtility.CrmData.RetrieveMultiple<Person>(query).ToList();
+            byte[] imageBytes = attachments[0].GetAttributeValue<byte[]>("entityimage");
+            string imgB = Convert.ToBase64String(imageBytes, 0, imageBytes.Length);
+            img.ImageUrl = "data:image/png;base64," + imgB;
+
+            ShowImage();
+            // this.Context.Response.Redirect(RedirectUrl);
+        }
+    }
+}
